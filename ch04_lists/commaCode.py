8a96cb7d0e49dba1bf3abb29fc491adb030aa37e
Daniel Davidson <r3dd3ath@gmail.com>
2018-05-12 09:14:14 -0500

    commit

--- ch04_lists/commaCode.py
+++ ch04_lists/commaCode.py
@@ -1,11 +1,10 @@
 spam = ['apples', 'bananas', 'tofu', 'cats']
-test = ['manjaro', 'ubuntu', 'linux mint', 'debian']
 
-def commaCode(list):
-	tempStr = ""
-	for i in range(len(list) - 1):
-		tempStr += "%s, " % list[i]
-	tempStr += 'and %s' % list[-1]
-	return tempStr
+def list_thing(words):
+     if len(words) == 1:
+         return words[0]
+     return '{} and {}'.format(', '.join(words[:-1]), words[-1])
 
-print commaCode(test) 
+print list_thing(spam[:1])
+
+print map(''.join, zip(*spam))
