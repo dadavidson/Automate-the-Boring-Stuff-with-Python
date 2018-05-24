523873af644ab9fd23d2e18215fb5bc8ad97be79
dadavidson <r3dd3ath@gmail.com>
2018-05-24 08:48:08 -0500

    ATBS repo commit

--- ch04_lists/commaCode.py
+++
@@ -1,8 +0,0 @@
-spam = ['apples', 'bananas', 'tofu', 'cats']
-
-def list_thing(words):
-     if len(words) == 1:
-         return words[0]
-     return '{} and {}'.format(', '.join(words[:-1]), words[-1])
-
-print list_thing(spam)
