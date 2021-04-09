2f5c9e44adc979c302e47b65b9a7def718d14956
danieldavidson <r3dd3ath@gmail.com>
2021-04-08 23:26:49 -0500

    Update ATBSWP

--- CH03/collatzSequence.py
+++ CH03/collatzSequence.py
@@ -1,30 +1,33 @@
-#! /usr/bin/python3
-# This is a programme that shows off the collatz sequence.
+#!/bin/python3
+
+# This is a programme performs the collatz sequence.
 
 # Determine collatz sequence
-def collatz(anyNumber):
-    if anyNumber % 2 == 0:
+def collatz(_number):
+    if _number % 2 == 0:
         # Even
-        return anyNumber // 2
-    elif anyNumber % 2 == 1:
+        return _number // 2
+    elif _number % 2 == 1:
         # Odd
-        return 3 * anyNumber + 1
+        return 3 * _number + 1
 
-# Check if user value is a valid integer number.
-# Will request untill true
+
+# Check if user value is a valid integer
+# Will request until true
 def userInput():
     while True:
         try:
-            return int(input())
+            # Ask the player to enter a number.
+            myNumber = int(input('Please select any number: \n'))
+
+            # Until value resolves to int(1) call collatz()
+            while myNumber != 1:
+                myNumber = collatz(myNumber)
+                print(myNumber)
             break
         except ValueError:
             print('[-] Error: Number not a valid integer number.')
-            print('[-] Please select a valid integer number...')
+            print('[-] Please select a valid integer number: \n')
 
 
-# Ask the player to guess a number.
-print('Please select any number...')
-myNumber = userInput()
-while myNumber != 1:
-    myNumber = collatz(myNumber)
-    print('-', myNumber)
+userInput()
